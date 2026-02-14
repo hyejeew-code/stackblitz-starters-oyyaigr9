1a43149a7f18793324b97bc32c3c7056600935eb
hyejeew-code <hyejeew@gmail.com>
2026-02-14 20:29:42 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# stackblitz-starters-oyyaigr9
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/hyejeew-code/stackblitz-starters-oyyaigr9)
---
+++ js/common-head.js
@@ -0,0 +1,10 @@
+const headContent = `
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width" />
+<link rel="stylesheet" href="../css/common.css" />
+<link rel="stylesheet" href="../css/font.css" />
+<link rel="stylesheet" href="../css/styles.css" />
+<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@24,400,0,0" />
+<script type="module" src="../js/script.js"></script>
+`;
+document.head.insertAdjacentHTML('afterbegin', headContent);
