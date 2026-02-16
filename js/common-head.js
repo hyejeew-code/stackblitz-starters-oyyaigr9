3a20db5ad61d644f7982387bf830206f743d5a9f
hyejeew-code <hyejeew@gmail.com>
2026-02-17 00:05:26 +0900

    cms페이지추가

--- js/common-head.js
+++ js/common-head.js
@@ -6,5 +6,6 @@ const headContent = `
 <link rel="stylesheet" href="../css/styles.css" />
 <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@24,400,0,0" />
 <script type="module" src="../js/script.js"></script>
+<script src="https://unpkg.com/feather-icons"></script>
 `;
 document.head.insertAdjacentHTML('afterbegin', headContent);
