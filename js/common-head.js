239821eb67e0f4053c8c37c9d9cdeb86e5a8b782
hyejeew-code <hyejeew@gmail.com>
2026-02-17 02:39:21 +0900

    css 수정

--- js/common-head.js
+++ js/common-head.js
@@ -6,7 +6,7 @@ const headContent = `
 <link rel="stylesheet" href="../css/styles.css" />
 <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@24,200,0,0" />
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-<script type="module" src="../js/script.js"></script>
+<script src="../js/script.js"></script>
 <script src="https://unpkg.com/feather-icons"></script>
 `;
 document.head.insertAdjacentHTML('afterbegin', headContent);
--- js/script.js
+++ js/script.js
@@ -1,9 +1,12 @@
-// 1. 먼저 "누구(a.item)"를 클릭할지 명확히 정해줍니다.
-$('.aside_depth01').click(function(e) {
-  e.preventDefault();
+$(document).ready(function() {
+  console.log("제이쿼리 연결 성공!"); // 이 메시지가 F12 콘솔에 뜨는지 보세요.
 
-  // 2. 이 안에서 비로소 $(this)는 "클릭한 바로 그 .item"이 됩니다!
-  // 3. 그리고 그 옆(next)에 있는 ul에 active를 붙입니다.
-  $('aside_depth01 > ul.aside_depth02').addClass('active');
-
+  $('.aside_depth01').off('click').on('click', function(e) {
+    e.preventDefault();
+    
+    // 혜지님이 강조하신 '클릭한 바로 그놈'의 자식 ul에 active 추가
+    $(this).find('ul.aside_depth02').addClass('active');
+    
+    console.log("클릭 작동함! active 추가됨.");
+  });
 });
