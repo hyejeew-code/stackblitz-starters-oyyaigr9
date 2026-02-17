5f50a6f21e5d430af4d80c69096d38aafc197446
null <null>
2026-02-18 05:24:44 +0900

    수정

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
-$(document).ready(function() {
+$(function() {
   $(".item").click(function(){
     $(this).next(".aside_depth02").stop().slideToggle(300);
     $(".aside_depth02").not($(this).next()).slideUp();
-  })
+  });
 });
