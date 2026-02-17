9c7a64cf6ccb4d71d04248ffc50ad35c27c4e097
hyejeew-code <hyejeew@gmail.com>
2026-02-18 05:04:44 +0900

    제이쿼리 추가

--- js/common-head.js
+++ js/common-head.js
@@ -5,7 +5,7 @@ const headContent = `
 <link rel="stylesheet" href="../css/font.css" />
 <link rel="stylesheet" href="../css/styles.css" />
 <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@24,200,0,0" />
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
 <script src="../js/script.js"></script>
 <script src="https://unpkg.com/feather-icons"></script>
 `;
--- js/script.js
+++ js/script.js
@@ -1,12 +1,6 @@
 $(document).ready(function() {
-  console.log("제이쿼리 연결 성공!"); // 이 메시지가 F12 콘솔에 뜨는지 보세요.
-
-  $('.aside_depth01').off('click').on('click', function(e) {
-    e.preventDefault();
-    
-    // 혜지님이 강조하신 '클릭한 바로 그놈'의 자식 ul에 active 추가
-    $(this).find('ul.aside_depth02').addClass('active');
-    
-    console.log("클릭 작동함! active 추가됨.");
-  });
+  $(".item").click(function(){
+    $(this).next(".aside_depth02").stop().slideToggle(300);
+    $(".aside_depth02").not($(this).next()).slideUp();
+  })
 });
