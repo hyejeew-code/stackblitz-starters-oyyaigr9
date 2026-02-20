a94605a05600f2c2b4d941b21e77dcbafd040f35
hyejeew-code <hyejeew@gmail.com>
2026-02-21 00:37:23 +0900

    이미지 추가

--- js/common-head.js
+++ js/common-head.js
@@ -1,12 +1,18 @@
-const headContent = `
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width" />
+function loadScript(src) {
+  const script = document.createElement('script');
+  script.src = src;
+  script.async = false; 
+  document.head.appendChild(script);
+}
+
+// jQuery를 먼저 로드하고, 그 다음 script.js 로드
+loadScript("https://code.jquery.com/jquery-3.7.1.min.js");
+loadScript("../js/script.js");
+
+const cssContent = `
 <link rel="stylesheet" href="../css/common.css" />
-<link rel="stylesheet" href="../css/font.css" />
 <link rel="stylesheet" href="../css/styles.css" />
+<link rel="stylesheet" href="../css/font.css" />
 <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@24,200,0,0" />
-<script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
-<script src="../js/script.js"></script>
-<script src="https://unpkg.com/feather-icons"></script>
 `;
-document.head.insertAdjacentHTML('afterbegin', headContent);
+document.head.insertAdjacentHTML('afterbegin', cssContent);
